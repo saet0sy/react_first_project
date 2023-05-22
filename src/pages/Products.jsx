f1f64e27531b0e18e57227928138574b3241d5ed
saet0sy <saet0sy@gmail.com>
2023-05-22 18:27:22 +0600

    updated

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,16 +1,31 @@
 import Navbar from "../components/Navbar";
-import Button from "../components/Button";
+import productsController from "../controller/products";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Products = () => {
-  const sayHello = () => {
-    alert("Hello");
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    const {data, status} = await productsController.getProducts();
+    alert(status)
+    console.log(data);
+    setProducts(data);
+    setLoading(false);
   };
 
   return (
     <div>
       <Navbar />
       <h1>Products</h1>
-      <Button text="Add to Cart" onClick={sayHello} />
+      <button onClick={fetchProducts}>fetch products</button>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        products.map((product) =><h1 key ={product.id}>{product.title} <br />{product.price}</h1>)
+      )}
     </div>
   );
 };
